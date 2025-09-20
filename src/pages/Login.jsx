168aaa78562cf56900b19ed506503392da5cc31a
Saatvik Rajvansshi <129985053+Saatvik2411@users.noreply.github.com>
2025-09-20 13:52:32 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -19,11 +19,15 @@ function Login() {
 
     setLoading(true);
     try {
-      const res = await fetch("http://localhost:5000/api/users/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(form),
-      });
+      const res = await fetch(
+  `${process.env.REACT_APP_API_URL}/api/users/login`,
+  {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(form),
+  }
+);
+
 
       const data = await res.json();
       if (res.ok) {
